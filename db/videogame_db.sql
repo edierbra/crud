ea3b57e454d24c82a179e8aca72594f46dd823a4
edierbra <edierbra@unicauca.edu.co>
2023-06-14 21:44:42 -0500

    Se actualizo la base de datos

--- db/videogame_db.sql
+++ db/videogame_db.sql
@@ -26,7 +26,7 @@ CREATE TABLE `categorias` (
   `id_categoria_pk` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) NOT NULL,
   PRIMARY KEY (`id_categoria_pk`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,7 +35,7 @@ CREATE TABLE `categorias` (
 
 LOCK TABLES `categorias` WRITE;
 /*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
-INSERT INTO `categorias` VALUES (1,'Acción'),(2,'Aventura'),(3,'Estrategia'),(4,'Deportes'),(5,'RPG'),(6,'Acción'),(7,'Aventura'),(8,'Estrategia'),(9,'Deportes'),(10,'RPG'),(11,'Acción'),(12,'Aventura'),(13,'Estrategia'),(14,'Deportes'),(15,'RPG'),(16,'Acción'),(17,'Aventura'),(18,'Estrategia'),(19,'Deportes'),(20,'RPG');
+INSERT INTO `categorias` VALUES (1,'Acción'),(2,'Aventura'),(3,'Estrategía'),(4,'Deportes'),(5,'RPG'),(6,'Aracade'),(7,'Simulacion'),(8,'Juegos de mesa'),(9,'Juegos musicales'),(10,'Horror'),(11,'Supervivencia'),(12,'Casual'),(13,'Rompecabezas'),(14,'Puzzle'),(15,'Carreras'),(16,'Disparos'),(17,'Lucha'),(18,'Sigilo'),(19,'Juegos de cartas'),(20,'Educacion'),(23,'Interactivosss'),(24,'Interactivas'),(25,'Musica');
 /*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -50,7 +50,7 @@ CREATE TABLE `plataformas` (
   `id_plataforma_pk` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) NOT NULL,
   PRIMARY KEY (`id_plataforma_pk`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -59,7 +59,7 @@ CREATE TABLE `plataformas` (
 
 LOCK TABLES `plataformas` WRITE;
 /*!40000 ALTER TABLE `plataformas` DISABLE KEYS */;
-INSERT INTO `plataformas` VALUES (1,'Xbox'),(2,'PlayStation'),(3,'Nintendo Switch'),(4,'PC'),(5,'Mobile'),(6,'PlayStation'),(7,'Xbox'),(8,'PC'),(9,'Nintendo Switch'),(10,'Mobile'),(11,'PlayStation'),(12,'Xbox'),(13,'PC'),(14,'Nintendo Switch'),(15,'Mobile'),(16,'PlayStation'),(17,'Xbox'),(18,'PC'),(19,'Nintendo Switch'),(20,'Mobile');
+INSERT INTO `plataformas` VALUES (1,'Xbox 2'),(2,'PlayStation'),(3,'Nintendo Switch'),(4,'PC'),(5,'Mobile'),(6,'Commodore 64'),(7,'Neo Geo'),(8,'PC Engine'),(9,'Atari 2600'),(10,'Super Nintendo Entertainment System'),(11,'Sega Dreamcast'),(12,'PlayStation Portable'),(13,'Nintendo 3DS'),(14,'Nintendo DS'),(15,'Game Boy Advance'),(16,'Game Boy Color'),(17,'Sega Genesis 2'),(18,'Wii U'),(19,'Wii'),(20,'PlayStation Vita'),(21,'Xbox 3'),(22,'Xbox 4');
 /*!40000 ALTER TABLE `plataformas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -105,7 +105,7 @@ CREATE TABLE `usuarios` (
   `clave` varchar(255) NOT NULL,
   `cargo` varchar(1) NOT NULL,
   PRIMARY KEY (`id_usuario_pk`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -114,7 +114,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'Edier Dario Bravo Bravo',25,'edierbra@gmail.com','202cb962ac59075b964b07152d234b70','1'),(2,'Juan Pablo Cuervo',30,'juanpablo@gmail.com','202cb962ac59075b964b07152d234b70','1'),(3,'Alice Smith',28,'alicesmith@gmail.com','202cb962ac59075b964b07152d234b70','1'),(4,'Bob Johnson',35,'bobjohnson@gmail.com','202cb962ac59075b964b07152d234b70','1'),(5,'Emily Davis',22,'emilydavis@gmail.com','202cb962ac59075b964b07152d234b70','1'),(6,'Michael Wilson',29,'michaelwilson@gmail.com','202cb962ac59075b964b07152d234b70','1'),(7,'Jessica Anderson',31,'jessicaanderson@gmail.com','202cb962ac59075b964b07152d234b70','1'),(8,'David Thompson',26,'davidthompson@gmail.com','202cb962ac59075b964b07152d234b70','1'),(9,'Sarah Brown',33,'sarahbrown@gmail.com','202cb962ac59075b964b07152d234b70','1'),(10,'Christopher Martinez',27,'christophermartinez@gmail.com','202cb962ac59075b964b07152d234b70','1'),(11,'Olivia Taylor',24,'oliviataylor@gmail.com','202cb962ac59075b964b07152d234b70','1'),(12,'Daniel Davis',32,'danieldavis@gmail.com','202cb962ac59075b964b07152d234b70','1'),(13,'Sophia Harris',29,'sophiaharris@gmail.com','202cb962ac59075b964b07152d234b70','1'),(14,'Matthew Clark',26,'matthewclark@gmail.com','202cb962ac59075b964b07152d234b70','1'),(15,'Ava Young',31,'avayoung@gmail.com','202cb962ac59075b964b07152d234b70','1'),(16,'James Lee',28,'jameslee@gmail.com','202cb962ac59075b964b07152d234b70','1'),(17,'Emma Lewis',25,'emmalewis@gmail.com','202cb962ac59075b964b07152d234b70','1'),(18,'Alexander Turner',34,'alexanderturner@gmail.com','202cb962ac59075b964b07152d234b70','1'),(19,'Mia Walker',27,'miawalker@gmail.com','202cb962ac59075b964b07152d234b70','1'),(20,'William Baker',30,'williambaker@gmail.com','202cb962ac59075b964b07152d234b70','1'),(21,'dario bravo',23,'dario@gmail.com','202cb962ac59075b964b07152d234b70','1'),(22,'daniela alvarado',34,'daniela@gmail.com','202cb962ac59075b964b07152d234b70','1'),(23,'Juan Pablo',45,'juan@gmail.com','202cb962ac59075b964b07152d234b70','1');
+INSERT INTO `usuarios` VALUES (1,'Edier Dario Bravo Bravo',25,'edierbra@gmail.com','202cb962ac59075b964b07152d234b70','1'),(2,'Juan Pablo Cuervo',30,'juanpablo@gmail.com','202cb962ac59075b964b07152d234b70','1'),(3,'Alice Smith',28,'alicesmith@gmail.com','202cb962ac59075b964b07152d234b70','1'),(4,'Bob Johnson',35,'bobjohnson@gmail.com','202cb962ac59075b964b07152d234b70','1'),(5,'Emily Davis',22,'emilydavis@gmail.com','202cb962ac59075b964b07152d234b70','1'),(6,'Michael Wilson',29,'michaelwilson@gmail.com','202cb962ac59075b964b07152d234b70','1'),(7,'Jessica Anderson',31,'jessicaanderson@gmail.com','202cb962ac59075b964b07152d234b70','1'),(8,'David Thompson',26,'davidthompson@gmail.com','202cb962ac59075b964b07152d234b70','1'),(9,'Sarah Brown',33,'sarahbrown@gmail.com','202cb962ac59075b964b07152d234b70','1'),(10,'Christopher Martinez',27,'christophermartinez@gmail.com','202cb962ac59075b964b07152d234b70','1'),(11,'Olivia Taylor',24,'oliviataylor@gmail.com','202cb962ac59075b964b07152d234b70','1'),(12,'Daniel Davis',32,'danieldavis@gmail.com','202cb962ac59075b964b07152d234b70','1'),(13,'Sophia Harris',29,'sophiaharris@gmail.com','202cb962ac59075b964b07152d234b70','1'),(14,'Matthew Clark',26,'matthewclark@gmail.com','202cb962ac59075b964b07152d234b70','1'),(15,'Ava Young',31,'avayoung@gmail.com','202cb962ac59075b964b07152d234b70','1'),(16,'James Lee',28,'jameslee@gmail.com','202cb962ac59075b964b07152d234b70','1'),(17,'Emma Lewis',25,'emmalewis@gmail.com','202cb962ac59075b964b07152d234b70','1'),(18,'Alexander Turner',34,'alexanderturner@gmail.com','202cb962ac59075b964b07152d234b70','1'),(19,'Mia Walker',27,'miawalker@gmail.com','202cb962ac59075b964b07152d234b70','1'),(20,'William Baker',30,'williambaker@gmail.com','202cb962ac59075b964b07152d234b70','1'),(38,'Sebastian',34,'sebastian@gmail.com','37693cfc748049e45d87b8c7d8b9aacd','1'),(39,'edier',23,'camila@gmail.com','202cb962ac59075b964b07152d234b70','1'),(40,'Paula Campo',34,'paula@gmail.com','202cb962ac59075b964b07152d234b70','1');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -137,7 +137,7 @@ CREATE TABLE `videojuegos` (
   KEY `id_categoria_fk` (`id_categoria_fk`),
   CONSTRAINT `videojuegos_ibfk_1` FOREIGN KEY (`id_plataforma_fk`) REFERENCES `plataformas` (`id_plataforma_pk`),
   CONSTRAINT `videojuegos_ibfk_2` FOREIGN KEY (`id_categoria_fk`) REFERENCES `categorias` (`id_categoria_pk`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -146,7 +146,7 @@ CREATE TABLE `videojuegos` (
 
 LOCK TABLES `videojuegos` WRITE;
 /*!40000 ALTER TABLE `videojuegos` DISABLE KEYS */;
-INSERT INTO `videojuegos` VALUES (1,'Halo Infinite',59.99,50.00,1,1),(2,'God of War',49.99,70.00,2,2),(3,'Super Mario Odyssey',49.99,55.00,3,2),(4,'The Witcher 3: Wild Hunt',39.99,80.00,4,5),(5,'FIFA 22',59.99,45.00,5,4),(6,'Red Dead Redemption 2',59.99,100.00,2,1),(7,'Minecraft',29.99,200.00,3,3),(8,'Call of Duty: Warzone',0.00,120.00,1,1),(9,'Animal Crossing: New Horizons',59.99,60.00,3,2),(10,'Assassin\'s Creed Valhalla',59.99,90.00,4,5),(11,'Fortnite',0.00,30.00,5,1),(12,'Resident Evil Village',59.99,45.00,2,1),(13,'League of Legends',0.00,25.00,4,3),(14,'Grand Theft Auto V',29.99,80.00,1,1),(15,'The Legend of Zelda: Breath of the Wild',59.99,70.00,3,2),(16,'Overwatch',39.99,50.00,4,1),(17,'NBA 2K22',59.99,95.00,5,4),(18,'Cyberpunk 2077',49.99,80.00,2,5),(19,'Pokémon Sword',59.99,55.00,3,2),(20,'Among Us',4.99,0.30,4,3);
+INSERT INTO `videojuegos` VALUES (1,'Halo Infinite',59.99,50.00,1,1),(2,'God of War',49.99,70.00,2,2),(3,'Super Mario Odyssey',49.99,55.00,3,2),(4,'The Witcher 3: Wild Hunt',39.99,80.00,4,5),(5,'FIFA 22',59.99,45.00,5,4),(6,'Red Dead Redemption 2',59.99,100.00,2,1),(7,'Minecraft',29.99,200.00,3,3),(8,'Call of Duty: Warzone',0.00,120.00,1,1),(9,'Animal Crossing: New Horizons',59.99,60.00,3,2),(10,'Assassin\'s Creed Valhalla',59.99,90.00,4,5),(11,'Fortnite',0.00,30.00,5,1),(12,'Resident Evil Village',59.99,45.00,2,1),(13,'League of Legends',0.00,25.00,4,3),(14,'Grand Theft Auto V',29.99,80.00,1,1),(15,'The Legend of Zelda: Breath of the Wild',59.99,70.00,3,2),(16,'Overwatch',39.99,50.00,4,1),(17,'NBA 2K22',59.99,95.00,5,4),(18,'Cyberpunk 2077',49.99,80.00,2,5),(19,'Pokémon Sword',59.99,55.00,3,2),(20,'Among Us',4.99,0.30,4,3),(66,'Comer',4000.00,333.00,20,16);
 /*!40000 ALTER TABLE `videojuegos` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -159,4 +159,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-12 18:41:40
+-- Dump completed on 2023-06-14 21:43:15
